651cd77c5e76d7cdc241add77cba1fd9261b3b0c
Chris Lee <chrislee187@gmail.com>
2019-06-05 14:28:41 +0100

    Add readme.md

--- src/pgn-tools.common.tests/Builders/FileSystemMocker.cs
+++ src/pgn-tools.common.tests/Builders/FileSystemMocker.cs
@@ -5,18 +5,18 @@ namespace pgn
 {
     public class FileSystemMocker
     {
+        public IFileSystemProvider Object => _mockFileSystem.Object;
         private readonly Mock<IFileSystemProvider> _mockFileSystem = new Mock<IFileSystemProvider>();
 
         public void VerifyGetFilesWasRecursive() => VerifyGetFiles(true, "*.pgn");
 
         public void VerifyGetFilesWasNotRecursive() => VerifyGetFiles(false, "*.pgn");
 
+
         public void SetupGetFiles(string[] strings, bool recurse = false, string pattern = "*.pgn") =>
             _mockFileSystem.Setup(fs => fs.GetFiles(It.IsAny<string>(), recurse, pattern))
                 .Returns(strings);
 
-        public IFileSystemProvider Object => _mockFileSystem.Object;
-         
         public void SetupDirectoryExists(bool value) =>
             _mockFileSystem.Setup(fs => fs.DirectoryExists(It.IsAny<string>()))
                 .Returns(value);
---
+++ src/readme.md
@@ -0,0 +1,3 @@
+# PGN Tools
+A set of tools and libraries to read [PGN Files](https://en.wikipedia.org/wiki/Portable_Game_Notation).
+
